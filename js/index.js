68d813699858732013838ae19328c7debadd23f4
sizem <muhammadxalilov726@gmail.com>
2024-11-30 14:45:55 +0500

    add: ringo

---
+++ js/index.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      mobile: "320px",
+      tablet: "640px",
+      largetablet: "1000px",
+      desktop: "1240px",
+    },
+    container: {
+      center: true,
+      screens: {
+        
+        tablet: "640px",
+        largetablet: "1000px",
+        desktop: "1240px",
+      },
+    },
+  },
+};
